c1cba3d1369993c858a1d0e71d2e875c95a846f1
yct <18202736439@163.com>
2019-05-02 13:48:59 +0800

    successfully regenerate the apk

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
+Asset;
 // import { Ionicons } from '@expo/vector-icons';
 import AppNavigator from './navigation/AppNavigator';
 
